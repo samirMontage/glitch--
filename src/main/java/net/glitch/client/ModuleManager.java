ecd7dfe03d911da45b5a622b1689b29d1f06b4d5
samirMontage <samir.worj@gmail.com>
2026-07-29 20:37:28 +0500

    Update ModuleManager.java

--- src/main/java/net/glitch/client/ModuleManager.java
+++ src/main/java/net/glitch/client/ModuleManager.java
@@ -1,25 +1,21 @@
 package net.glitch.client;
 
-import net.glitch.client.module.ClickGuiModule;
+import net.glitch.client.module.Module;
+import net.glitch.client.module.Category;
 import net.glitch.client.module.combat.AutoTotem;
 import net.glitch.client.module.combat.KillAura;
+import net.glitch.client.module.ClickGuiModule;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ModuleManager {
     private final List<Module> modules = new ArrayList<>();
 
     public ModuleManager() {
-        // Регистрируем все наши модули
-        register(new ClickGuiModule());
-        register(new AutoTotem());
-        register(new KillAura());
-    }
-
-    private void register(Module module) {
-        modules.add(module);
+        modules.add(new AutoTotem());
+        modules.add(new KillAura());
+        modules.add(new ClickGuiModule());
     }
 
     public List<Module> getModules() {
@@ -27,8 +23,12 @@ public class ModuleManager {
     }
 
     public List<Module> getModulesByCategory(Category category) {
-        return modules.stream()
-                .filter(m -> m.getCategory() == category)
-                .collect(Collectors.toList());
+        List<Module> matched = new ArrayList<>();
+        for (Module module : modules) {
+            if (module.getCategory() == category) {
+                matched.add(module);
+            }
+        }
+        return matched;
     }
 }
