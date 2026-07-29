f4da5422b06d2e226a28d25ecfe209d76ffb6b01
samirMontage <samir.worj@gmail.com>
2026-07-29 20:34:36 +0500

    Update ClickGuiModule.java

--- src/main/java/net/glitch/client/module/ClickGuiModule.java
+++ src/main/java/net/glitch/client/module/ClickGuiModule.java
@@ -1,25 +1,16 @@
 package net.glitch.client.module;
 
-import net.glitch.client.Category;
-import net.glitch.client.Module;
-import net.glitch.client.ui.ClickGuiScreen;
-
 public class ClickGuiModule extends Module {
 
     public ClickGuiModule() {
-        // Имя, Описание, Категория, Стандартная клавиша (344 = Right Shift)
-        super("ClickGUI", "Открывает меню управления модулями", Category.RENDER, 344);
+        super("ClickGUI", Category.RENDER);
     }
 
     @Override
     public void onEnable() {
-        System.out.println("Opening Glitch ClickGUI...");
-        // В реальном клиенте здесь открывается Screen в Майнкрафте:
-        // mc.openScreen(new ClickGuiScreen());
     }
 
     @Override
     public void onDisable() {
-        System.out.println("Closing Glitch ClickGUI...");
     }
 }
