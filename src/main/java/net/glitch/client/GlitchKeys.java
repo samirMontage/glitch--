8f3e7d38eec916a4766ea02fc36082925dcf8ef6
samirMontage <samir.worj@gmail.com>
2026-07-29 22:19:22 +0500

    Update GlitchKeys.java

--- src/main/java/net/glitch/client/GlitchKeys.java
+++ src/main/java/net/glitch/client/GlitchKeys.java
@@ -1,15 +1,15 @@
 package net.glitch.client;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.options.KeyBinding; // Если ругается на options, замени на option в зависимости от маппингов, но лучше так:
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class GlitchKeys {
-    public static net.minecraft.client.options.KeyBinding openGuiKey;
+    public static KeyBinding openGuiKey;
 
     public static void register() {
-        openGuiKey = KeyBindingHelper.registerKeyBinding(new net.minecraft.client.options.KeyBinding(
+        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "key.glitch.open_gui",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_RIGHT_SHIFT,
