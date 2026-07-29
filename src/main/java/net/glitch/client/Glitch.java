276efe59a14dc8126727441e4d5248db8516c3d9
samirMontage <samir.worj@gmail.com>
2026-07-29 19:39:23 +0500

    Update Glitch.java

--- src/main/java/net/glitch/client/Glitch.java
+++ src/main/java/net/glitch/client/Glitch.java
@@ -3,9 +3,6 @@ package net.glitch.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import net.glitch.client.module.ModuleManager;
 import net.glitch.client.ui.ClickGuiScreen;
 import org.lwjgl.glfw.GLFW;
@@ -13,31 +10,29 @@ import org.lwjgl.glfw.GLFW;
 public class Glitch implements ModInitializer, ClientModInitializer {
     public static Glitch INSTANCE;
     private ModuleManager moduleManager;
-    public static KeyBinding clickGuiKey;
+    private boolean isKeyPressed = false;
 
     @Override
     public void onInitialize() {
-        // Оставляем пустым для main эндпоинта
+        INSTANCE = this;
+        moduleManager = new ModuleManager();
     }
 
     @Override
     public void onInitializeClient() {
-        INSTANCE = this;
-        moduleManager = new ModuleManager();
-
-        // 1. Создаем бинд
-        clickGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.glitch.clickgui",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_RIGHT_SHIFT,
-                "category.glitch"
-        ));
-
-        // 2. Проверяем нажатие каждый тик клиента
+        // Отслеживаем прямое физическое нажатие Правого Shift (GLFW_KEY_RIGHT_SHIFT = 301)
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null && client.currentScreen == null) {
-                if (clickGuiKey.wasPressed()) {
-                    client.openScreen(new ClickGuiScreen());
+            if (client.getWindow() != null) {
+                long windowHandle = client.getWindow().getHandle();
+                boolean down = GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_RIGHT_SHIFT) == GLFW.GLFW_PRESS;
+
+                if (down && !isKeyPressed) {
+                    isKeyPressed = true;
+                    if (client.currentScreen == null) {
+                        client.execute(() -> client.openScreen(new ClickGuiScreen()));
+                    }
+                } else if (!down) {
+                    isKeyPressed = false;
                 }
             }
         });
