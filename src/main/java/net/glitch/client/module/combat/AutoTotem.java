c8864e560f25ccc72ecb114296348f5f9d358c61
samirMontage <samir.worj@gmail.com>
2026-07-29 20:34:09 +0500

    Update AutoTotem.java

--- src/main/java/net/glitch/client/module/combat/AutoTotem.java
+++ src/main/java/net/glitch/client/module/combat/AutoTotem.java
@@ -2,22 +2,19 @@ package net.glitch.client.module.combat;
 
 import net.glitch.client.module.Category;
 import net.glitch.client.module.Module;
-import net.minecraft.item.Items;
-import net.minecraft.screen.slot.SlotActionType;
 
 public class AutoTotem extends Module {
 
     public AutoTotem() {
-        super("AutoTotem", "Автоматически берет тотем в руку", Category.COMBAT, 0);
+        super("AutoTotem", Category.COMBAT);
     }
 
     @Override
     public void onEnable() {
-        super.onEnable();
+        if (!isEnabled()) return;
     }
 
     @Override
     public void onDisable() {
-        super.onDisable();
     }
 }
