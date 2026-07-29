366ae4f19267b5b7784850b224cb75944de529a1
samirMontage <samir.worj@gmail.com>
2026-07-29 21:23:57 +0500

    Update Glitch.java

--- src/main/java/net/glitch/client/Glitch.java
+++ src/main/java/net/glitch/client/Glitch.java
@@ -1,41 +1,27 @@
 package net.glitch.client;
 
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.glitch.client.module.ModuleManager;
-import net.glitch.client.ui.ClickGuiScreen;
-import org.lwjgl.glfw.GLFW;
+import net.glitch.client.ModuleManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Glitch implements ModInitializer, ClientModInitializer {
-    public static Glitch INSTANCE;
+public class Glitch implements ModInitializer {
+    public static final String MOD_ID = "glitch";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    
+    private static Glitch INSTANCE;
     private ModuleManager moduleManager;
-    private boolean isKeyPressed = false;
 
     @Override
     public void onInitialize() {
         INSTANCE = this;
+        LOGGER.info("Initializing Glitch Client...");
+        
         moduleManager = new ModuleManager();
     }
 
-    @Override
-    public void onInitializeClient() {
-        // Отслеживаем прямое физическое нажатие Правого Shift (GLFW_KEY_RIGHT_SHIFT = 301)
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.getWindow() != null) {
-                long windowHandle = client.getWindow().getHandle();
-                boolean down = GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_RIGHT_SHIFT) == GLFW.GLFW_PRESS;
-
-                if (down && !isKeyPressed) {
-                    isKeyPressed = true;
-                    if (client.currentScreen == null) {
-                        client.execute(() -> client.openScreen(new ClickGuiScreen()));
-                    }
-                } else if (!down) {
-                    isKeyPressed = false;
-                }
-            }
-        });
+    public static Glitch getInstance() {
+        return INSTANCE;
     }
 
     public ModuleManager getModuleManager() {
