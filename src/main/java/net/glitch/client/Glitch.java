70bb61a2f44f73d92109e3ce61bed9b7bad40891
samirMontage <samir.worj@gmail.com>
2026-07-29 18:38:23 +0500

    Update Glitch.java

--- src/main/java/net/glitch/client/Glitch.java
+++ src/main/java/net/glitch/client/Glitch.java
@@ -1,11 +1,19 @@
 package net.glitch.client;
 
-public class Glitch {
-    public static final Glitch INSTANCE = new Glitch();
-    public static final String NAME = "Glitch Client";
-    public static final String VERSION = "1.0.0";
+import net.fabricmc.api.ModInitializer;
+import net.glitch.client.module.ModuleManager;
 
-    public void init() {
-        System.out.println("[" + NAME + "] Loading client version " + VERSION);
+public class Glitch implements ModInitializer {
+    public static Glitch INSTANCE;
+    private ModuleManager moduleManager;
+
+    @Override
+    public onInitialize() {
+        INSTANCE = this;
+        moduleManager = new ModuleManager();
+    }
+
+    public ModuleManager getModuleManager() {
+        return moduleManager;
     }
 }
