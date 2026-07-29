898b5670295334fc5a416475618cc30bf6e51085
samirMontage <samir.worj@gmail.com>
2026-07-29 18:47:52 +0500

    Update ModuleManager.java

--- src/main/java/net/glitch/client/module/ModuleManager.java
+++ src/main/java/net/glitch/client/module/ModuleManager.java
@@ -1,7 +1,26 @@
 package net.glitch.client.module;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModuleManager {
+    private final List<Module> modules = new ArrayList<>();
+
     public ModuleManager() {
-        // Инициализация модулей
+        // Здесь будущие модули (KillAura, AutoTotem и т.д.)
+    }
+
+    public List<Module> getModules() {
+        return modules;
+    }
+
+    public List<Module> getModulesByCategory(Category category) {
+        List<Module> categoryModules = new ArrayList<>();
+        for (Module module : modules) {
+            if (module.getCategory() == category) {
+                categoryModules.add(module);
+            }
+        }
+        return categoryModules;
     }
 }
